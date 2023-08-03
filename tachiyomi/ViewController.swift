6e6860f5791182b792b675897347fcdbf2a51c89
mlsg-gracemy <grace.yu@monstar-lab.com>
2023-08-03 12:35:32 -0700

    Initial Commit

---
+++ tachiyomi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tachiyomi
+//
+//  Created by Grace, Mu-Hui Yu on 8/3/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
